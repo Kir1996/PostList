52ae29c85faca4134cf0128476be11f2c469972d
Kir1996 <115106673+Kir1996@users.noreply.github.com>
2024-02-02 14:52:36 +0300

    error

--- src/API/PostServise.js
+++ src/API/PostServise.js
@@ -4,6 +4,9 @@ export default class PostService{
     static async getAll(){
         const response= await axios.get('https://jsonplaceholder.typicode.com/posts')
         return response.data
+        .catch(error => {
+            console.error('There was an error!', error);
+          });
     }
     static async getByID(id){
         const response= await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
