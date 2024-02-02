400d29979437455992b97bdafb5dc3bee6513e4f
Kir1996 <115106673+Kir1996@users.noreply.github.com>
2024-02-02 14:56:59 +0300

    api-2

--- src/API/PostServise.js
+++ src/API/PostServise.js
@@ -1,20 +1,33 @@
-import axios from "axios"
+import axios from "axios";
 
-export default class PostService{
-    static async getAll(){
-        const response= await axios.get('https://jsonplaceholder.typicode.com/posts')
-        return response.data
-        .catch(error => {
-            console.error('There was an error!', error);
-          });
+export default class PostService {
+  static async getAll() {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      return response.data;
+    } catch (error) {
+      console.error('There was an error!', error);
+      throw error; 
     }
-    static async getByID(id){
-        const response= await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        return response.data
+  }
+
+  static async getByID(id) {
+    try {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error('There was an error fetching the post by ID!', error);
+      throw error;
     }
-    static async getCommentsByID(id){
-        const response= await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-        return response.data
+  }
+
+  static async getCommentsByID(id) {
+    try {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+      return response.data;
+    } catch (error) {
+      console.error('There was an error fetching the comments by post ID!', error);
+      throw error;
     }
+  }
 }
-
