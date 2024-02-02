1befe4989b2afb3e0849cac5b8bd9140276f7f67
Kir1996 <115106673+Kir1996@users.noreply.github.com>
2024-02-02 14:50:01 +0300

    api

--- src/API/PostServise.js
+++ src/API/PostServise.js
@@ -2,15 +2,15 @@ import axios from "axios"
 
 export default class PostService{
     static async getAll(){
-        const response= await axios.get('http://jsonplaceholder.typicode.com/posts')
+        const response= await axios.get('https://jsonplaceholder.typicode.com/posts')
         return response.data
     }
     static async getByID(id){
-        const response= await axios.get(`http://jsonplaceholder.typicode.com/posts/${id}`)
+        const response= await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         return response.data
     }
     static async getCommentsByID(id){
-        const response= await axios.get(`http://jsonplaceholder.typicode.com/posts/${id}/comments`)
+        const response= await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
         return response.data
     }
 }
